c848a5c08467ff3aa8c83ab2d4f75455bb6206ac
ahmad856 <31070424+ahmad856@users.noreply.github.com>
2017-10-10 06:18:11 +0500

    Update Loan.java

--- Loan.java
+++ Loan.java
@@ -1,16 +1,20 @@
 public class Loan {
     protected Borrower br;
     protected Book bk;
-    protected myDate issue;
     protected myDate due;
- 
-    
+    protected myDate issue;
+
     public Loan(Book bk, Borrower br, myDate i) {
         this.bk = bk;
         this.br = br;
-        this.issue = i;
+        this.issue=i;
         this.due = i.add(7);
     }
-
-
+    
+    public Loan(Book bk, Borrower br, myDate d,myDate i) {
+        this.bk = bk;
+        this.br = br;
+        this.due=d;
+        this.issue = i;
+    }
 }
