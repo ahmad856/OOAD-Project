5b643974acdff4f62a9e4159759d834185c109c0
ahmad856 <31070424+ahmad856@users.noreply.github.com>
2017-10-10 05:57:44 +0500

    Create LMS.java

---
+++ LMS.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+public class LMS {
+
+ArrayList<Book> collection;
+ArrayList<Borrower> borrowers;
+
+public LMS(){
+    collection=new ArrayList<>();
+    borrowers=new ArrayList<>();
+}
+
+public boolean searchByName(String name){
+
+    return true;
+}
+
+public boolean searchByAuthor(String name){
+
+    return true;
+}
+
+public boolean searchBySubject(String name){
+
+    return true;
+}
+}
