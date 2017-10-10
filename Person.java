a458b33d52b95026c564c58e5b8c662f143c5546
ahmad856 <31070424+ahmad856@users.noreply.github.com>
2017-10-10 06:05:57 +0500

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,15 @@
+public abstract class Person {
+    
+    protected String name;
+    protected String address;
+    protected String phone;
+    protected LMS lib;
+
+    public Person(String n,String a,String p){
+        name = n;
+        address = a;
+        phone = p;
+        lib=null;
+    }
+    
+}
