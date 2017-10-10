cbd8f2f9b1cf4e3d1eb789e4d04351c5e5f275d1
ahmad856 <31070424+ahmad856@users.noreply.github.com>
2017-10-10 06:20:07 +0500

    Update Staff.java

--- Staff.java
+++ Staff.java
@@ -1,11 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public abstract class Staff extends Person{
-    public Staff(String n,String a,String p){
-        super(n,a,p);
+    
+    public Staff(String n,String a,String p,String pass){
+        super(n,a,p,pass);
     }
+    
+    public Staff(String n,String a,String p,String pass,int id,LMS l){
+        super(n,a,p,pass,id,l);
+    }
+    
+    
     //borrower use cases
-    public Book searchByName(String name){
-        
-        return lib.searchByName(name);
+    
+ 
+    public Book getByID(int id){
+    
+        return lib.getByID(id);
+    }
+    
+    public void searchByID(int id){
+    
+        lib.searchByID(id);
+    }
+    
+    public void searchByName(String name){
+    
+        lib.searchByName(name);
     }
     public void searchByAuthor(String author){
         
@@ -18,7 +43,7 @@ public abstract class Staff extends Person{
     
     public void BorrowForBorrower(Borrower br,Book bk,myDate dt){
         
-        if(bk.hold){
+        if(bk.isHeld()){
             System.out.println("\nThis book is currently held by another borrower. Added to your waiting list.");
             br.waitingList.add(bk);return;
         }
@@ -26,12 +51,13 @@ public abstract class Staff extends Person{
         
     }
     
-    public void displayBorrowerInfo(Borrower br){br.printInfo();}
-    public void displayBorrowerInfoAndCurrentBooks(Borrower br){br.printInfoAndCurrentBooks();}
-    public void displayBorrowerWaitingList(Borrower br){br.printWaitingList();}
+    public void printBorrowerInfo(Borrower br){br.printInfo();}
+    public void printBorrowerInfoAndCurrentBooks(Borrower br){br.printInfoAndCurrentBooks();}
+    public void printBorrowerWaitingList(Borrower br){br.printWaitingList();}
     
-    public void addBorrowerBalance(Borrower br,int amount){br.balance+=amount;}
-    public int getBorrowerBalance(Borrower br){return br.balance;}
+    public void payForBorrower(Borrower br,int amount,myDate dt){br.pay(amount, dt);}
+    public int getBorrowerFine(Borrower br){return br.getFine();}
+    public void printBorrowerFineHistory(Borrower br){br.printFineHistory();}
     
     
     ///Clerk use cases
@@ -39,26 +65,37 @@ public abstract class Staff extends Person{
     
     public void checkIn(Borrower br, Book bk, myDate dt){
         
-        if(!bk.hold){System.out.println("\nThis book is currently not issued to anyone.");return;}
+        if(!bk.isHeld()){System.out.println("\nThis book is currently not issued to anyone.");return;}
         
         for(int i=0;i<br.loans.size();i++) {
             if(br.loans.get(i).bk==bk){
                 
                 if(dt.isGreater(br.loans.get(i).due)){
                     //fine
-                    int fine=50*(dt.difference(br.loans.get(i).due));
-                    br.balance-=fine;
-                    System.out.println("\nThe book '"+bk.name+"' has been returned by the borrower "
-                            +br.name+" (ID : "+br.bid+") "+dt.difference(br.loans.get(i).due)+" DAYS LATE with fine amount "+fine+".");
+
+                    int fine=50*(br.loans.get(i).due.difference(dt));
+                    br.fineLoans.add(br.loans.get(i));
+                    br.fineDates.add(dt);
+                    br.fines.add(fine);
                     
+                    
+                    System.out.println("\nThe book '"+bk.name+"' has been returned by the borrower "+br.name+" (ID : "+br.pid+") "+dt.difference(br.loans.get(i).due)+" DAYS LATE. Due fine is "+fine+".");  
+                }
+                
+                try {
+                    Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/ooad","ahmad","69824");
+                    Statement stmt = con.createStatement();
+                    stmt.executeUpdate("delete from ooad.loan where PersonID = "+br.loans.get(i).br.pid+" and BookID = "+br.loans.get(i).bk.bkid);
+                }
+                catch (SQLException ex) {
+                    Logger.getLogger(OOADAssignment1.class.getName()).log(Level.SEVERE, null, ex);
                 }
                 
                 br.loans.remove(br.loans.get(i));
-                bk.hold=false;
+                bk.loan=null;
                 System.out.println("\nThe book '"+bk.name+"' has been returned by the borrower "
-                        +br.name+" (ID : "+br.bid+") within due date.");
-                return;
-                
+                        +br.name+" (ID : "+br.pid+") within due date.");
+                return;   
             }
         }
         System.out.println("\nThis book is not issued to the specified borrower.");
@@ -66,27 +103,34 @@ public abstract class Staff extends Person{
     }
     
     public void checkOut(Borrower br, Book bk,myDate dt){
-        
-        if(br.balance<0){System.out.println("\nBook can not be issued unless borrower pays due fines.");return;}
-        bk.hold=true;
+        if(br.getFine()>0){System.out.println("\nBook can not be issued unless borrower pays due fines.");return;}
+        bk.loan=null;
         Loan l=new Loan(bk,br,dt);
         br.loans.add(l);
-        System.out.print("\nThe book '"+bk.name+"' has been issued to the borrower "+br.name+" (ID : "+br.bid+"). "
+        bk.loan=l;
+        System.out.print("\nThe book '"+bk.name+"' has been issued to the borrower "+br.name+" (ID : "+br.pid+"). "
                 +"Return it by ");
         dt.add(7).printDate();
         System.out.println(".");
+        //Update DB
+        try {
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/ooad","ahmad","69824");
+            Statement stmt = con.createStatement();
+            stmt.executeUpdate("INSERT INTO ooad.loan(PersonID,BookID,DueDate,IssueDate)"+"VALUES('"+l.br.pid+"','"+l.bk.bkid+"','"+l.due.getDateObject()+"','"+l.issue.getDateObject()+"')");
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(OOADAssignment1.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     public Borrower getBorrower(int id){
-        
         Borrower b=null;
         for(int i=0;i<lib.persons.size();i++){
             
             Person p=lib.persons.get(i);
             if(p instanceof Borrower){
-                
                 b=(Borrower)p;
-                if(b.bid==id)break;
+                if(b.pid==id)break;
                 else b=null;
             }
         }
@@ -97,14 +141,17 @@ public abstract class Staff extends Person{
         lib.addBorrower(b);
     }
     
+    public void deleteBorrower(Borrower b){
+        lib.deleteBorrower(b);
+    }
     
-    public void updateBorrower(Borrower b,String t,String ad){
-        lib.updateBorrower(b, t, ad);
+    public void updateBorrower(Borrower b,String name,String address,String phone,String password){
+        lib.updateBorrower(b, name, address, phone, password);
     }
     
     public void renewLoan(Borrower br, Book bk, myDate dt){
     
-        if(bk.hold){
+        if(bk.isHeld()){
             System.out.println("\nThis book is not held by this borrower.");
             return;
         }
@@ -118,7 +165,7 @@ public abstract class Staff extends Person{
             else b=null;
             
         }
-        if(b==null){
+        if(b==null || l==null){
             System.out.println("\nThis book is not held by this borrower.");
             return;
         }
@@ -127,20 +174,12 @@ public abstract class Staff extends Person{
             return;
         }
         
-        if(br.balance<0){System.out.println("\nLoan can not be renewed unless borrower pays due fines.");return;}
+        if(br.getFine()>0){System.out.println("\nLoan can not be renewed unless borrower pays due fines.");return;}
         
         l.due=dt.add(7);
-        System.out.print("\nLoan renewed, The book '"+bk.name+"' has been issued to the borrower "+br.name+" (ID : "+br.bid+") again. "
+        System.out.print("\nLoan renewed, The book '"+bk.name+"' has been issued to the borrower "+br.name+" (ID : "+br.pid+") again. "
                 +"Return it by ");
         l.due.printDate();
         System.out.println(".");
-    }
-    
-    public void clearBookHistory(Book bk){
-    
-        if(bk.hold){System.out.println("\nBook is currently issued. Operation Failed.");return;}
-        bk.loans.clear();
-        System.out.println("\nBook history cleared.");
-    }
-    
+    }   
 }
