5b495ebbf4eb82d8a8f3e47fc8ab4146c33f5a1f
ahmad856 <31070424+ahmad856@users.noreply.github.com>
2017-10-10 05:59:31 +0500

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,42 @@
+public class Book {
+    
+    protected String name;
+    protected String author;
+    protected String subject;
+    protected ArrayList<Loan> loans;
+    protected LMS lib;
+    protected boolean hold;
+    
+    public Book(String name, String author, String subject) {
+        this.name = name;
+        this.author = author;
+        this.subject = subject;
+        this.loans=new ArrayList<>();
+        this.hold = false;
+    }
+    
+    public boolean isHeld(){return hold;}
+    
+    public void printHistory(){
+    
+        System.out.println("");
+        System.out.println("Loan history of book '"+name+"' by "+author+" :");
+        System.out.println("");
+        for(int i=0;i<loans.size();i++){
+        
+            System.out.print("Issued by "+loans.get(i).br.name+" on ");
+            loans.get(i).issue.printDate();
+            System.out.println(".");
+        }
+    }
+    
+    public void printInfo(){
+    
+        System.out.println("");
+        System.out.println("Name : "+name);
+        System.out.println("Author : "+author);
+        System.out.println("Subject : "+subject);
+        System.out.println("Currently held? : "+hold);
+    }
+    
+}
