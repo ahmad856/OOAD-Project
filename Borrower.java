c485bcfb3e93db951f03c0fb17a9b1283af664f0
ahmad856 <31070424+ahmad856@users.noreply.github.com>
2017-10-10 06:14:49 +0500

    Update Borrower.java

--- Borrower.java
+++ Borrower.java
@@ -1,23 +1,97 @@
+import java.util.ArrayList;
+import java.util.Random;
 public class Borrower extends Person{
     
     protected ArrayList<Loan> loans;
     protected ArrayList<Book> waitingList;       //hold the books which are loaned by someone else
-    protected int finepaid;
-    protected int bid;
-    protected int balance;
+    protected int fine;
     protected static int noOfBorrowers=0;
+    protected ArrayList<myDate> fineDates;
+    protected ArrayList<Integer> fines;
+    protected ArrayList<Loan> fineLoans;
+
     
-    public Borrower(String n,String a,String p){
-    
-        super(n,a,p);
-        bid=noOfBorrowers;
-        noOfBorrowers++;
-        balance=0;
+    public void pay(int amount,myDate dt){
+        
+        if(amount<1){System.out.println("Payment failed.");return;}
+
+        fine-=amount;
+        fineDates.add(dt);
+        fines.add(-1*amount);
+        fineLoans.add(null);
+        System.out.println("Payment done.");
     }
     
-    public Book searchByName(String name){
+    public int getFine(){return fine;}
     
-        return lib.searchByName(name);
+    protected void printFineHistory(){
+        
+        System.out.println("");
+        System.out.println("Account history of "+name+" (ID : "+pid+") is as follows : ");
+        System.out.println("");
+        
+        for(int i=0;i<fines.size();i++){
+            
+            int acc=0;
+            System.out.print((i+1)+". Date : ");
+            fineDates.get(i).printDate();
+            if(fines.get(i)<0){
+                acc-=fines.get(i);
+                System.out.println(" Payment : "+(-1*fines.get(i))+" Total Fine : "+acc);
+            }
+                
+            else {
+                acc+=fines.get(i);
+                System.out.print(" Fine : Issued book '"+fineLoans.get(i).bk.name
+                        +"' (Book ID : "+fineLoans.get(i).bk.bkid+") on ");
+                fineLoans.get(i).issue.printDate();
+                System.out.println(" and returned "+fineLoans.get(i).issue.difference(fineDates.get(i))
+                        +" days late. Fine : "+fines.get(i)+" Total Fine : "+acc);
+            }
+        }
+        System.out.println("Due fine is : "+fine+".");
+
+    }
+    
+    
+    public Borrower(String n,String a,String p,String pass){
+    
+        super(n,a,p,pass);
+        noOfBorrowers++;
+        
+        loans=new ArrayList<>();
+        waitingList=new ArrayList<>();
+        fineDates=new ArrayList<>();
+        fines=new ArrayList<>();
+        fineLoans=new ArrayList<>();
+    }
+    
+    public Borrower(String n,String a,String p,String pass,int id,LMS l){
+    
+        super(n,a,p,pass,id,l);
+        noOfBorrowers++;
+        
+        loans=new ArrayList<>();
+        waitingList=new ArrayList<>();
+        fineDates=new ArrayList<>();
+        fines=new ArrayList<>();
+        fineLoans=new ArrayList<>();
+    }
+    
+    
+    public Book getByID(int id){
+    
+        return lib.getByID(id);
+    }
+    
+    public void searchByID(int id){
+    
+        lib.searchByID(id);
+    }
+    
+    public void searchByName(String name){
+    
+        lib.searchByName(name);
     }
     public void searchByAuthor(String author){
     
@@ -30,11 +104,11 @@ public class Borrower extends Person{
     
     public void Borrow(Book bk,myDate dt){
         
-        if(bk.hold){
+        if(bk.isHeld()){
             System.out.println("\nThis book is currently held by another borrower. Added to your waiting list.");
             waitingList.add(bk);return;
         }
-        
+        /*
         Random gen=new Random();
         int cid=gen.nextInt(Clerk.noOfClerks);
         Clerk c=null;
@@ -50,6 +124,7 @@ public class Borrower extends Person{
         }
         if(c!=null)c.checkOut(this,bk,dt);
         else System.out.println("No Clerk Available.");
+        */
         
     }
     
@@ -63,11 +138,12 @@ public class Borrower extends Person{
         for (Book bk : waitingList) {
             bk.printInfo();
         }
-    
     }
     
     public void printInfo(){
         System.out.println("");
+        System.out.println("ID : "+pid);
+        System.out.println("Password : "+password);
         System.out.println("Name : "+name);
         System.out.println("Address : "+address);
         System.out.println("Phone : "+phone);
@@ -86,8 +162,8 @@ public class Borrower extends Person{
         printInfo();
         listCurrentBooks();
     }
+//1) The borrower can place hold requests
+//2) The system shall maintain requests in FIFO order
+//3) Any user can view the hold-request queue for a given book
     
-    public int getCount(){return noOfBorrowers;}
-    public void addBalance(int amount){balance+=amount;}
-    public int getBalance(){return balance;   
 }
