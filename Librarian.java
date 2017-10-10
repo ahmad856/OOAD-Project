746ee5001765a1566765bef03aff2d842e1b8eb4
ahmad856 <31070424+ahmad856@users.noreply.github.com>
2017-10-10 06:17:26 +0500

    Update Librarian.java

--- Librarian.java
+++ Librarian.java
@@ -1,23 +1,73 @@
 public class Librarian extends Staff{
     
-    protected int lid;
     private static int noOfLibrarians=0;
     
-    public Librarian(String n,String a,String p){
+    public Librarian(String n,String a,String p,String pass,LMS lib,int id){
     
-        super(n,a,p);
-        lid=noOfLibrarians;
+        super(n,a,p,pass,id,lib);
         noOfLibrarians++;
     }
     
+    public Librarian(String n,String a,String p,String pass,LMS lib){
+    
+        super(n,a,p,pass);
+        noOfLibrarians++;
+        this.lib=lib;
+        lib.persons.add(this);
+    }
+   
+    
     //clerk use cases
+     
+    
+    public Clerk getClerk(int id){
+        
+        Clerk c=null;
+        for(int i=0;i<lib.persons.size();i++){
+            
+            Person p=lib.persons.get(i);
+            if(p instanceof Clerk){
+                
+                c=(Clerk)p;
+                if(c.pid==id)break;
+                else c=null;
+            }
+        }
+        return c;
+    }
+    public Librarian getLibrarian(int id){
+        
+        Librarian l=null;
+        for(int i=0;i<lib.persons.size();i++){
+            
+            Person p=lib.persons.get(i);
+            if(p instanceof Librarian){
+                
+                l=(Librarian)p;
+                if(l.pid==id)break;
+                else l=null;
+            }
+        }
+        return l;
+    }
     
     public void addClerk(Clerk cl){
     
         lib.addClerk(cl);
     }
-
     
+    public void deleteClerk(Clerk cl){
+    
+        lib.deleteClerk(cl);
+    }
+    
+    public void addLibrarian(Librarian ln){
+        lib.addLibrarian(ln);
+    }
+    public void deleteLibrarian(Librarian ln){
+	lib.deleteLibrarian(ln);
+    }
+
     public void addBook(Book b){
         lib.addBook(b);
     }
@@ -26,8 +76,8 @@ public class Librarian extends Staff{
         lib.deleteBook(b);
     }
     
-    public void updateBook(Book b,String n,String a,String s){
-        lib.updateBook(b, n, a, s);
+    public void updateBook(Book b,String name,String author,String subject){
+        lib.updateBook(b, name, author, subject);
     }
     
     public int getCount(){return noOfLibrarians;}
