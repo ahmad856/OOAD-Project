f40ba21d4206466d78f857e0f6560e4cbb5c8dc3
ahmad856 <31070424+ahmad856@users.noreply.github.com>
2017-10-10 06:18:58 +0500

    Update Person.java

--- Person.java
+++ Person.java
@@ -4,12 +4,29 @@ public abstract class Person {
     protected String address;
     protected String phone;
     protected LMS lib;
+    protected int pid;
+    protected String password;
+    protected static int noOfPersons=0;
 
-    public Person(String n,String a,String p){
+    public Person(String n,String a,String p,String pass){
         name = n;
         address = a;
         phone = p;
         lib=null;
+        pid=noOfPersons;
+        noOfPersons++;
+        password=pass;
     }
     
+    public Person(String n,String a,String p,String pass, int id,LMS l){
+        name = n;
+        address = a;
+        phone = p;
+        lib=null;
+        pid=id;
+        noOfPersons++;
+        password=pass;
+        lib=l;
+        l.persons.add(this);
+    }
 }
